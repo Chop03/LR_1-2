fd49c7da654b5d552dcbe5f4a363b4559fc1d68f
Chop03 <144237862+Chop03@users.noreply.github.com>
2023-09-10 21:07:23 +0300

    Add files via upload

---
+++ b3-13.pas
@@ -0,0 +1,8 @@
+﻿program blok3zad18;
+var x: integer;
+begin
+    writeln('Введите ваш год: ');
+    readln(x);
+    if (x mod 4 <> 0) or (x mod 100 = 0) and (x mod 400 <> 0) then writeln('Ваш год - не високосный')
+    else writeln('Ваш год - високосный');
+end.
---
+++ b3-14.pas
@@ -0,0 +1,13 @@
+﻿program Blok3zad14;
+Var a,b,c:integer;
+Begin
+Writeln('Введите три числа: ');
+read(a,b,c);
+
+if (a < b) and (b < c) then write('Числа по возрастанию: ',a, ' ', b, ' ', c);
+if (a < c) and (c < b) then write('Числа по возрастанию: ',a, ' ', c, ' ', b);
+if (b < a) and (a < c) then write('Числа по возрастанию: ',b, ' ', a, ' ', c);
+if (b < c) and (c < a) then write('Числа по возрастанию: ',b, ' ', c, ' ', a);
+if (c < b) and (b < a) then write('Числа по возрастанию: ',c, ' ', b, ' ', a);
+if (c < a) and (a < b) then write('Числа по возрастанию: ',c, ' ', a, ' ', b);
+end.
