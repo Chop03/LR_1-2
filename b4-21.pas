3ace06e123dad26805e3a48e9fb56a97f06e0bfe
Chop03 <144237862+Chop03@users.noreply.github.com>
2023-09-11 11:15:38 +0300

    Add files via upload

--- b4-21.pas
+++ b4-21.pas
@@ -3,9 +3,9 @@ var a,b,c:integer;
 begin
   writeln('Введите три числа: ');
   read(a,b,c); 
-  if ((a = 0) and (b = 0) and (c = 0))then writeln('NO') 
+  if ((a = 0) and (b = 0) and (c = 0))then writeln('Нет чётного и нечётного числа') 
     else if (((a mod 2 = 0) or (b mod 2 = 0) or (c mod 2 = 0)) and 
     ((a mod 2 <> 0) or (b mod 2 <> 0) or (c mod 2 <> 0))) 
-    then writeln('YES') 
-        else writeln('NO'); 
+    then writeln('Есть чётное и нечётное числа') 
+        else writeln('Нет чётного и нечётного числа'); 
 end.
