d43b36c2b5066fe81d5cc8202273deff61d3d743
Chop03 <144237862+Chop03@users.noreply.github.com>
2023-09-11 10:04:20 +0300

    Add files via upload

---
+++ b3-17.pas
@@ -0,0 +1,10 @@
+﻿program blok3zad17;
+var a,b,c,D,x1,x2:real;
+begin
+  writeln('Введите a,b,c: ');
+  read(a,b,c);
+  D:=sqr(b)-4*a*c;
+  x1:= (-b)-sqrt(d)/2*a;
+  x2:= (-b)+sqrt(d)/2*a;
+  writeln('Корень уравнения х1: ',x1, 'Корень уравнения х2: ',x2);
+end.
