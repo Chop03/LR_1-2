aab1ed4aa86a107715f73743448fa600ced8a342
Chop03 <144237862+Chop03@users.noreply.github.com>
2023-09-11 11:12:02 +0300

    Add files via upload

--- b4-22.pas
+++ b4-22.pas
@@ -1,7 +1,8 @@
 ﻿program blok4zad22;
 var x1,x2,y1,y2:integer;
 begin
+writeln('Введите координаты: ');
 read(x1,x2,y1,y2);
-if (x1=y1) or (x2=y2) then writeln('YES')
-else writeln('NO');
+if (x1=y1) or (x2=y2) then writeln('Ладья бьет эту фигуру')
+else writeln('Ладья не бьет эту фигуру');
 end.
